4831228976fad230f7b2232dffe4acdcb095bfb8
unknown <ed.f.jones@gmail.com>
2020-05-02 13:22:54 -0400

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <p>Hello from todo item</p>
+        )
+    }
+}

d9d340a86d06061b58c82939147ae5c62a5ef44e
unknown <ed.f.jones@gmail.com>
2020-05-02 16:17:43 -0400

    worked on handle and submit methods

--- src/App.js
+++ src/App.js
@@ -8,21 +8,30 @@ import TodoList from './components/TodoList';
 
 class App extends Component {
   state ={
-    items:[{id:1, title: 'wake up'},
-            { id:2, title: "make breakfast"}
-  ],
+    items:[],
   id: uuidv4(),
   item: '',
   editItem: false
   };
-  handleChange= (e)=> {
-    console.log('handleChange')
-  };
+  
+  handleChange= (e) => {
+    this.setState({
+      item: e.target.value
+    });
+  }
 
   handleSubmit = (e)=> {
-    console.log('handelSubmit')
+    e.preventDefault();
+    const newItem = {
+      id:this.state.id,
+      title:this.state.item
+    }
+    const updatedItems =[...this.state.items, newItem]
+    this.setState({
+      items:updatedItems, item:'', id:uuidv4(), editItem:false
+    }, ()=> console.log(this.state))
   };
-
+  
   clearList = () => {
     console.log('clear list')
   };
@@ -34,7 +43,6 @@ class App extends Component {
     console.log('edit edit ${id}')
   }
   render(){
-    console.log(this.state)
     return(
       <div className="container">
           <div className="row">
